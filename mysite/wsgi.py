3e426fbdb4fd2d7609e8a66fb5157ae84af380aa
yurii <makogonyury@gmail.com>
2016-02-28 20:43:46 +0200

    Remove wsgi pyramid from appache config

--- mysite/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import logging.config
-logging.config.fileConfig(
-    os.environ.get('LOGGING_CONFIG', '/usr/share/mysite/mysite.logging.rest.conf'))
-
-
-from mysite.server.wsgi import application
